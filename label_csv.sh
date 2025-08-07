b8258c61673ef7d559ac79ab467e6d81fc18a8d2
Jiashuo Zhang <sethzhangjs@gmail.com>
2025-08-07 17:19:15 -0400

    update

--- label_csv.sh
+++ label_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=label_csv
-#SBATCH --output=/home/szhan81/chexpert-labeler/demo/logs/label_csv_%j_%x.out
-#SBATCH --error=/home/szhan81/chexpert-labeler/demo/logs/label_csv_%j_%x.err
+#SBATCH --output=/home/szhan81/chexpert-labeler/logs/label_csv_%j_%x.out
+#SBATCH --error=/home/szhan81/chexpert-labeler/logs/label_csv_%j_%x.err
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=1
 
@@ -11,8 +11,8 @@ conda activate chexpert-label
 cd /home/szhan81/chexpert-labeler
 
 python -u label_csv.py\
-    --reports_path "/home/szhan81/chexpert-labeler/demo/data/Atelectasis_mention_sample_analysis.csv"\
-    --output_path "/home/szhan81/chexpert-labeler/demo/data/result/Atelectasis_mention_sample_analysis_results.csv"\
+    --reports_path /home/szhan81/chexpert-labeler/demo/data/input/Atelectasis_mention_sample_analysis.csv\
+    --output_path /home/szhan81/chexpert-labeler/demo/data/output/Atelectasis_mention_sample_analysis_results.csv\
     --batch_size 10 \
     --enable_segmentation \
     --segment_length 350 \
